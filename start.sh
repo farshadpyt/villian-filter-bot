a198e9f2db6083793eda8ed9e0058e999a39b17f
farshadpyt <89522593+farshadpyt@users.noreply.github.com>
2022-02-09 21:08:11 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Cloning Repo, Please Wait..."
-git clone -b master https://github.com/josprojects/tgmoviebot.git /tgmoviebot
-cd /tgmoviebot
+git clone -b master https://github.com/farshadpyt/pro-evamaria.git /tgmoviebot
+cd /pro-evamaria
 echo "Installing Requirements..."
 pip3 install -U -r requirements.txt
 echo "Starting Bot, Please Wait..."
